d4c7909a730e3e0132720f08b3e812eba92bc0df
ZurielTovar <110933004+ZurielTovar@users.noreply.github.com>
2022-09-27 20:08:01 -0500

    Update exercise.py

--- assignments/15SumaColumnas/src/exercise.py
+++ assignments/15SumaColumnas/src/exercise.py
@@ -1,5 +1,29 @@
 def main():
     #escribe tu código abajo de esta línea
+    r = int(input())
+    c = int(input())
+    if r > 0 and c > 0:
+        suma = 0
+        matriz = []
+        resultados = []
+        for i in range (r):
+            renglones = []
+            for j in range (c):
+                num = int(input())
+                renglones.append(num)
+            matriz.append(renglones)
+
+        for j in range (c):
+            suma = 0
+            for l in range (r):
+                suma = suma + matriz[l][j]
+            resultados.append(suma)
+
+        print(resultados)
+    else:
+        print("Error")
+    
+    
 
 
 
